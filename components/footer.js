cff47dcc4a8b9d63fbb876c624f12847772f47e0
KhushbuNikhare <khushbunikhare@gmail.com>
2021-12-19 16:40:41 +0530

    final project

---
+++ components/footer.js
@@ -0,0 +1,69 @@
+function footer(){
+
+    return ` <hr/>
+     <div id="footer">
+    <div id="main_div">
+     
+     <div id="div2">
+         <h6>Coursera</h6>
+         <a href="https://about.coursera.org/">About</a><br>
+         <a href="https://about.coursera.org/how-coursera-works/">What we offer</a><br>
+         <a href="https://about.coursera.org/leadership">Leadership</a><br>
+         <a href="https://about.coursera.org/careers/">Careers</a><br>
+         <a href="https://www.coursera.org/browse">Catalog</a><br>
+         <a href="https://www.coursera.org/professional-certificates">Professional certificate</a><br>
+         <a href="https://www.coursera.org/mastertrack">MasterTrack™</a><br>
+         <a href="https://www.coursera.org/degrees">Degree</a><br>
+         <a href="https://www.coursera.org/business?utm_campaign=website&utm_content=corp-to-home-footer-for-enterprise&utm_medium=coursera&utm_source=enterprise">Enterprises</a><br>
+         <a href="https://www.coursera.org/government?utm_campaign=website&utm_content=corp-to-home-footer-for-government&utm_medium=coursera&utm_source=enterprise">For government</a><br>
+         <a href="https://www.coursera.org/campus?utm_campaign=website&utm_content=corp-to-home-footer-for-campus&utm_medium=coursera&utm_source=enterprise">For campus</a><br>
+         <a href="https://www.coursera.org/business/teams?utm_campaign=b2b-website&utm_content=for-teams-footer-enterprise&utm_medium=coursera&utm_source=enterprise">For Team</a><br>
+     </div>
+     <div id="div3">
+         <h6>Community</h6>
+         <a href="https://www.coursera.support/s/community?language=en_US">Learners</a><br>
+         <a href="https://www.coursera.org/about/partners">Partners</a><br>
+         <a href="https://www.coursera.org/in">Developers</a><br>
+         <a href="https://www.coursera.org/in">Beta tester</a><br>
+         <a href="https://www.coursera.support/s/feed/0D51U00003BmIp9SAF?language=en_US">Translators</a><br>
+         <a href="https://medium.com/coursera-engineering">Tech Blog</a><br> 
+         <a href="https://www.coursera.org/teaching-center/">Teaching center</a><br>
+     </div>
+    
+     <div id="div5">
+         <h6>More</h6>
+         <a href="https://about.coursera.org/press">Press</a><br>
+         <a href="https://investor.coursera.com/overview/default.aspx">Investers</a><br>
+         <a href="https://investor.coursera.com/overview/default.aspx">Terms</a><br>
+         <a href="https://www.coursera.org/about/privacy">Privacy</a><br>
+         <a href="https://www.coursera.support/s/learner-help-center?language=en_US">Help</a><br>
+         <a href="">Accessibility</a><br>
+         <a href="https://www.coursera.org/about/contact">Contact</a><br>
+         <a href="https://www.coursera.org/directory">Directory</a><br>
+         <a href="https://about.coursera.org/affiliates">Affiliates</a><br>
+     </div>
+
+     <div id="div1">
+        
+          <a href="https://apps.apple.com/app/apple-store/id736535961"> <img id="apple"src="https://d3njjcbhbojbot.cloudfront.net/web/images/icons/download_on_the_app_store_badge_en.svg"><br><img id="google" src="https://d3njjcbhbojbot.cloudfront.net/web/images/icons/en_generic_rgb_wo_45.png" alt=""></a><br>
+          <img id="bing"src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d3njjcbhbojbot.cloudfront.net/web/images/icons/2018-B-Corp-Logo-Black-S.png?auto=format%2Ccompress&dpr=1&w=151&h=120&q=40" alt="">
+          
+     </div>
+     
+    </div>
+    <hr>
+    <div id="div4">
+    <p>© 2021 Coursera Inc. All rights reserved.</p>
+     <div id="links">
+     <a href="https://www.facebook.com/Coursera"><img src="https://cdn-icons.flaticon.com/png/128/2168/premium/2168281.png?token=exp=1639827530~hmac=c37ef1981005713bf515c44edccee163"></a>
+     <a href="https://www.linkedin.com/company/coursera/"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384014.png"></a>
+     <a href="https://twitter.com/coursera"><img src="https://cdn-icons.flaticon.com/png/128/2168/premium/2168336.png?token=exp=1639827729~hmac=93ac107de8df7c53eb150159ab85a1b4"></a>
+     <a href="https://www.youtube.com/user/coursera"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384012.png"></a>
+     <a href="https://www.instagram.com/coursera/"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384015.png"></a>
+    </div>
+ </div>
+   
+ </div>`
+}
+
+export default footer;
